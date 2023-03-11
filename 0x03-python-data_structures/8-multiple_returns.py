2426e7a2b1d2771a08cb74a7fe5ac8d169cc4278
abaynew-guadie <abaynew@yahoo.com>
2023-03-11 13:58:45 +0000

    task 8:More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-
-# 8-multiple_returns.py#
-
 def multiple_returns(sentence):
-
-"""Returns the length of a string and its first character."""
-
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    length = len(sentence)
+    first = ""
+    if not sentence:
+        first = None
+    else:
+        first = sentence[0]
+    return(length, first)
