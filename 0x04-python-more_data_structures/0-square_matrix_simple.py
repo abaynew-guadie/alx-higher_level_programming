6662ac1cccb7729ddf199b69ec84296a00c5a2fa
abaynew-guadie <abaynewhu@yahoo.com>
2023-03-15 03:06:29 -0700

    task 0:Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in (matrix):
-            new_matrix.append([x*x for x in row])
-    return new_matrix
+    return list(map(lambda submat: list(map(lambda e: e**2, submat)), matrix))
