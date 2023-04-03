559f9c265ad5b730e60421cf9a886609f0354b07
abaynew-guadie <abaynew@yahoo.com>
2023-04-03 07:25:52 -0700

    task 10:nqeens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,29 +1,52 @@
 #!/usr/bin/python3
-
-# N queens puzzle
-""""
-    This program solves the N queens problem
-"""
+'''Module for N Queens problem.'''
 
 
-import sys
+def isSafe(board, row, col):
+    '''Checks if position is safe from attack.
 
-size = len(sys.argv)
-N = sys.argv[1]
+    Args:
+        board: The board state.
+        row: The row to check.
+        col: The colum to check.
+    '''
+    for c in range(col):
+        if board[c] is row or abs(board[c] - row) is abs(c - col):
+            return False
+    return True
 
-def nqueens(size, N)
-    if size != 2:
+
+def checkBoard(board, col):
+    '''Checks the board state column by column using backtracking.
+
+    Args:
+        board: The board state.
+        col: The current colum to check.
+    '''
+    n = len(board)
+    if col is n:
+        print(str([[c, board[c]] for c in range(n)]))
+        return
+
+    for row in range(n):
+        if isSafe(board, row, col):
+            board[col] = row
+            checkBoard(board, col + 1)
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-    if not isinstance(N, int):
-        ("N must be a number")
-        exit(1)
-    if N < 4:
-        ("N must be at least 4")
-        exit(1)
-    else:
-        return [
-                y + x
-                for y in nqueens(1, N)
-                for x in nqueens(N + 1, N
-            ]
+        sys.exit(1)
+    n = 0
+    try:
+        n = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    board = [0 for col in range(n)]
+    checkBoard(board, 0)
