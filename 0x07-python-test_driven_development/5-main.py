3aaa7d5ba22c9adb5765d32706d3d19eaf7f0028
abaynew-guadie <abaynew@yahoo.com>
2023-04-03 08:37:29 -0700

    tests

--- 0x07-python-test_driven_development/5-main.py
+++ 0x07-python-test_driven_development/5-main.py
@@ -1,33 +1,12 @@
 #!/usr/bin/python3
-# Author: 
+text_indentation = __import__('5-text_indentation').text_indentation
 
-"""
-    Defines a function that prints a text with 2
-    new lines after each of a specified character.
-"""
-
-
-def text_indentation(text):
-    """
-        Prints text with 2 new lines after each of
-        these characters '.' '?' and ':'
-
-        param: text(str) - the text to be searched for characters
-    """
-    if type(text) != str:
-        raise TypeError("text must be a string")
-
-    x = 0
-    while x < len(text) and text[x] == " ":
-        x += 1
-
-    while x < len(text):
-        print(text[x], end='')
-        if text[x] == "\n" or text[x] in ".?:":
-            if text[x] in ".?:":
-                print("\n")
-            x += 1
-            while x < len(text) and text[x] == " ":
-                x += 1
-            continue
-        x += 1
+text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
