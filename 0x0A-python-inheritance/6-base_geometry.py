2069c7e0ccab6c31f0dba592c2779ace7cdcea77
abaynew-guadie <abaynew@yahoo.com>
2023-04-10 05:35:28 -0700

    task 6:Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Auth: Abaynew G
+
+""" This module defines a class based on 5-base_geometry.py ."""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    def area(self):
+        """unimplemneted area method """
+        raise Exception("area() is not implemented")
